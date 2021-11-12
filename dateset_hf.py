f6dffeb108515d7307773112482d4d8f81ba9442
lihang19980418 <1686914440@qq.com>
2021-11-12 16:02:06 +0800

    from qmj 注释

--- dateset_hf.py
+++ dateset_hf.py
@@ -8,33 +8,9 @@ from sklearn.preprocessing import scale
 from scipy import signal
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader
-# import utils.resample
-#from scipy import signal
 
 
-#
-# class DatasetECG(Dataset):
-#     def __init__(self, data_path,train_list,train_label):
-#         self.data_path=data_path
-#         self.train_label=train_label
-#         self.train_list=train_list
-#
-#     def __len__(self):
-#         return len(self.train_list)
-#
-#     def __getitem__(self,idx):
-#         npy_name=os.path.join(self.data_path,self.train_list[idx])
-#         array=resample(np.load(npy_name))
-#
-#         mean_temp=np.mean(array,axis=0)
-#         std_temp=np.std(array,axis=0)
-#         if 0 in std_temp:
-#             print(array)
-#
-#         return (array-mean_temp)/std_temp,int(self.train_label[idx])
-
-
-class Dataset_DIASTOLIC(Dataset):
+class Dataset_DIASTOLIC(Dataset):#dadaset 重采样，归一化
     def __init__(self, data_path,train_list,train_label):
         self.data_path=data_path
         self.train_label=train_label
@@ -43,11 +19,11 @@ class Dataset_DIASTOLIC(Dataset):
     def __len__(self):
         return len(self.train_list)
 
-    def get_statis_result(self,array):
-        channel,length=array.shape
-        min=np.min(array,axis=1)
-        max=np.max(array,axis=1)
-        temp_array=(array-min.reshape(channel,1))/(max-min).reshape(channel,1)
+    def get_statis_result(self,array):#数据归一化
+        channel,length = array.shape#shape代表维数，channel代表几个通道，length代表长度
+        min = np.min(array,axis=1)#每行的最小值
+        max = np.max(array,axis=1)#每行的最大值
+        temp_array = (array-min.reshape(channel,1)) / (max-min).reshape(channel,1)#reshape将数组重新组织
         return temp_array
 
     def _resample(self,array):
@@ -60,18 +36,16 @@ class Dataset_DIASTOLIC(Dataset):
         return resampled_array
 
     def __getitem__(self,idx):
-        patient_name=os.path.join(self.data_path,self.train_list[idx])  #
+        patient_name=os.path.join(self.data_path,self.train_list[idx])
         all_npy=os.listdir(patient_name)
 
         npy_name = os.path.join(patient_name, all_npy[0])
-        #print(npy_name)
             
         array = np.load(npy_name,allow_pickle=True)
-        #array = self._resample(array)
+
         normalizated_array=self.get_statis_result(array)
         normalizated_array=self._resample(normalizated_array)
-        #return np.expand_dims((array-mean_temp)/std_temp,axis=1) ,float(self.train_label[idx])
-        return normalizated_array.transpose(1,0) ,float(self.train_label[idx])
+        return normalizated_array.transpose(1,0) ,float(self.train_label[idx])#转置
 
 
 
